e08857b5d7f6c05ef6ef263f0509e823477dcd16
Renan <nrenan2007@gmail.com>
2025-10-07 11:59:44 -0300

    ADO 3 pela metade

---
+++ Aula3/ADO3/4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("PROGRAMA DE VERIFICAÇÃO DE NÚMERO PAR");
+
+        Console.WriteLine("Digite um número inteiro: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar.");
+        }
+    }
+}
