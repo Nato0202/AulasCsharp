0723dbc1ae9ecf79360899a52321bd6b99d5bacb
Nato0202 <nrenan2007@gmail.com>
2025-10-14 21:00:40 +0000

    leves ajustes

---
+++ Chose.cs
@@ -0,0 +1,36 @@
+namespace AulasCsharp;
+
+public class Choice
+{
+    public static int Choice()
+    {
+        do
+        {
+            Console.WriteLine("ESCOLHA UMA DAS SEÇÕES DE ATIVIDADE");
+        } while (choice1 != 11);
+    }
+
+    public static int ChoiceADO1()
+    {
+        do
+        {
+            Console.Writeline("ESCOLHA UMA DAS ATIVIDADES");
+        } while (choice2 != 11);
+    }
+
+    public static int ChoiceADO2()
+    {
+        do
+        {
+            Console.Writeline("ESCOLHA UMA DAS ATIVIDADES");
+        } while (choice2 != 11);
+    }
+
+    public static int ChoiceADO3()
+    {
+        do
+        {
+            Console.Writeline("ESCOLHA UMA DAS ATIVIDADES");
+        } while (choice2 != 11);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AulasCsharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("------------------");
+        Console.WriteLine("MENU DE ATIVIDADES");
+        Console.WriteLine("------------------");
+        //aqui seria a importação das funções de escolha do arquivo chose.cs
+    }
+}
